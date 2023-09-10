83bb4107ea280ba930f52825b6c0412ca295d920
9A3LET <112285190+croleo@users.noreply.github.com>
2023-09-10 21:17:37 +0200

    Add files via upload

---
+++ header.h
@@ -0,0 +1,5 @@
+#ifndef __AVR_ATmega328P__
+	#define __AVR_ATmega328P__
+#endif
+
+#define F_CPU 16000000UL
---
+++ main.c
@@ -0,0 +1,17 @@
+#include "header.h"
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+
+    DDRB = 0x20;
+
+    while(1)
+    {
+        PORTB = 0;
+		_delay_ms(500);
+		PORTB = 0x20;
+		_delay_ms(80);
+    }
+}
